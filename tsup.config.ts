2dc785fa07bb62b044629e950668e82f5b975d98
grind-t <grindt.gd@gmail.com>
2025-07-03 22:36:06 +0300

    init

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  name: "@grind-t/lib",
+  name: "@grind-t/cbr-ratings",
   entry: ["lib/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
