a96efaac7512b902e812e3b79f848f8ed0212b56
grind-t <grindt.gd@gmail.com>
2025-07-13 19:25:30 +0300

    add schema

--- lib/search/types/output.ts
+++ lib/search/types/output.ts
@@ -1,30 +1,45 @@
-export type RatingItem = {
-  ratingAction: string;
-  country: string;
-  koNumber: string;
-  releaseDate: string;
-  inn: string;
-  objectType: string;
-  ratingValue: string;
-  prediction: string;
-  objectName: string;
-  kraName: string;
-  releaseUrl: string;
-  objectId: string;
-  isin: string;
-  subjectName: string;
-};
+import { z } from "zod";
 
-export type SearchRatingResponse = {
-  status: string; // 'success' | 'error'
-  data: {
-    pageCount: number;
-    pageNumber: number;
-    sortingField: string;
-    sortingDirection: string;
-    pageSize: number;
-    itemList: RatingItem[];
-    itemCount: number;
-  } | null;
-  errors: { message: string; code: number; customData: any }[];
-};
+export const RatingItemSchema = z.object({
+  ratingAction: z.string(),
+  country: z.string(),
+  koNumber: z.string(),
+  releaseDate: z.string(),
+  inn: z.string(),
+  objectType: z.string(),
+  ratingValue: z.string(),
+  prediction: z.string(),
+  objectName: z.string(),
+  kraName: z.string(),
+  releaseUrl: z.string(),
+  objectId: z.string(),
+  isin: z.string(),
+  subjectName: z.string(),
+});
+
+export const SearchRatingDataSchema = z.object({
+  pageCount: z.number(),
+  pageNumber: z.number(),
+  sortingField: z.string(),
+  sortingDirection: z.string(),
+  pageSize: z.number(),
+  itemList: z.array(RatingItemSchema),
+  itemCount: z.number(),
+});
+
+export const SearchRatingResponseSchema = z.object({
+  status: z.enum(["success", "error"]),
+  data: SearchRatingDataSchema.nullable(),
+  errors: z.array(
+    z.object({
+      message: z.string(),
+      code: z.number(),
+      customData: z.any(),
+    })
+  ),
+});
+
+// Экспорт типов TypeScript, сгенерированных из Zod схем
+export type RatingItem = z.infer<typeof RatingItemSchema>;
+export type SearchRatingData = z.infer<typeof SearchRatingDataSchema>;
+export type SearchRatingResponse = z.infer<typeof SearchRatingResponseSchema>;
