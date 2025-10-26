d4f11ba7b5d9af99a96c8b407aadebaa6560d647
grind-t <grindt.gd@gmail.com>
2025-10-26 16:41:18 +0300

    fix

--- lib/index.ts
+++ lib/index.ts
@@ -5,3 +5,19 @@ export {
 } from "./search/helpers/convertRatingValue.ts";
 export { isRatingWithdrawn } from "./search/helpers/isRatingWithdrawn.ts";
 export { searchRatings } from "./search/index.ts";
+export type {
+	Country,
+	KraName,
+	RatingAction,
+	RatingGroup,
+	RatingScale,
+	RatingStatus,
+	SearchRatingFields,
+	SearchRatingRequest,
+	TypeGroup,
+} from "./search/types/input.ts";
+export type {
+	RatingItem,
+	SearchRatingData,
+	SearchRatingResponse,
+} from "./search/types/output.ts";
